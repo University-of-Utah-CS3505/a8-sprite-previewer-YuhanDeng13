9317e441e08e96b7236cce0447dbaea38d3dec28
YuhanDeng13 <1819961979@qq.com>
2025-04-15 19:06:24 -0600

    Add files via upload
    basic layout

--- SpritePreviewer.py
+++ SpritePreviewer.py
@@ -27,6 +27,9 @@ class SpritePreview(QMainWindow):
 
         # Add any other instance variables needed to track information as the program
         # runs here
+        self.start_value = 30
+        self.start_frames = 0
+        self.is_playing = False
 
         # Make the GUI in the setupUI method
         self.setupUI()
@@ -36,16 +39,54 @@ class SpritePreview(QMainWindow):
         # An application needs a central widget - often a QFrame
         frame = QFrame()
 
+        main_layout = QVBoxLayout()
+
+        layout1 = QHBoxLayout()
+
+        # Sprite image display
+        self.image_label = QLabel()
+        self.image_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.image_label.setFixedSize(150, 150)
+        layout1.addWidget(self.image_label)
+
+        self.slider = QSlider(Qt.Orientation.Vertical)
+        self.slider.setMinimum(0)
+        self.slider.setMaximum(100)
+
+        self.slider.setTickPosition(QSlider.TickPosition.TicksBothSides)
+        self.slider.setTickInterval(10)
+
+        layout1.addWidget(self.slider)
+
+        main_layout.addLayout(layout1)
+
+        layout2 = QHBoxLayout()
+        self_fps_text = QLabel("Frames per second")
+        layout2.addWidget(self_fps_text)
+
+        self_fps_value= QLabel(str(self.start_value))
+        layout2.addWidget(self_fps_value)
+        main_layout.addLayout(layout2)
+
+        layout3 = QHBoxLayout()
+
+        self.start_bottom = QPushButton("Start")
+        layout3.addWidget(self.start_bottom)
+        main_layout.addLayout(layout3)
+
+
         # Add a lot of code here to make layouts, more QFrame or QWidgets, and
         # the other components of the program.
+
+
+
         # Create needed connections between the UI components and slot methods
         # you define in this class.
-
+        frame.setLayout(main_layout)
         self.setCentralWidget(frame)
 
 
     # You will need methods in the class to act as slots to connect to signals
-
 
 def main():
     app = QApplication([])
